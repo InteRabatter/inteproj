bd9b4c7669dccf1a6069c34437ae4c60a6a82633
MisoINTE <afro0793@student.su.se>
2014-10-29 16:25:19 +0100

    Added several changes to the doc
    Almost done now.

--- src/inteproj/Inteproj.java
+++ src/inteproj/Inteproj.java
@@ -3,7 +3,7 @@ package inteproj;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Inteproj {
+public class Inteproj{
 
 	public static void main(String[] args) {
 		// Our little testing prog
